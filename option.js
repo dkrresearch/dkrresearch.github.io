a6ce52514db47b361447e46e4192c956b5687e3e
Dana <dana@nobrolla.com>
2022-09-02 11:28:48 -0700

    added open button

--- option.js
+++ option.js
@@ -12,6 +12,9 @@ async function loadOptionData() {
     jsonOptionInfo = await fetchOptionTable();
     jsonOptionTableInfo = findOptionInfo(jsonOptionInfo);
 
+    console.log(jsonInfo)
+    console.log(jsonOptionTableInfo)
+
     let symbol = jsonOptionTableInfo.symbol
     link = "<a href='/symbol.html?symbol="+symbol+"'>"+symbol+"</a>"
     document.querySelector('#symbol').innerHTML = link;
@@ -116,20 +119,101 @@ async function fetchOptionTable() {
 function loadOpenPosition() {
     bid = parseFloat(jsonOptionTableInfo.quote.bid)
     document.getElementById("details_open_price").value = (bid + 0.01).toFixed(2)
-    onMarginChange()
+    onSliderChange()
 }
 
-function onMarginChange() {
+function onSliderChange() {
     var slider = document.getElementById("myRange");
 
     document.getElementById("details_margin_label").innerHTML = "$" + slider.value + "K";
 
     let shares = (slider.value * 1000.0) / jsonOptionTableInfo.strike_price
     let contracts = shares / 100
-    document.getElementById("details_contracts").innerHTML = contracts.toFixed(0);
+    document.getElementById("details_contracts").value = contracts.toFixed(0);
+    onMarginChange();
+}
 
+function onMarginChange() {
+    let contracts = parseFloat( document.getElementById("details_contracts").value )
+    let shares = contracts * 100
     let commision = parseFloat( document.getElementById("details_commision_price").value )
     let open_price = parseFloat( document.getElementById("details_open_price").value )
     let details_total_proceeds = (shares * open_price) -  commision
     document.getElementById("details_total_proceeds").innerHTML = "$" + details_total_proceeds.toFixed(2);
+}
+
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+
+  
+async function onOpenPosition() {
+    
+    document.getElementById("open_button").style.backgroundColor = "#888";
+    document.getElementById("open_button").disabled = true;
+    document.getElementById("open_button").innerHTML = "Opening Position ...";
+
+    let info = {}
+    info["symbol"] = jsonOptionTableInfo["symbol"]
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    let open_date = yyyy+"-"+mm+"-"+dd;
+    info['open_date'] = open_date
+    info['open_dte'] = parseInt( jsonOptionTableInfo['dte'] )
+    info['open_price'] = parseFloat( document.getElementById("details_open_price").value )
+    info['open_pol'] = parseFloat( jsonOptionTableInfo['price_of_loss'] )
+
+    info['expiration_date'] = jsonOptionTableInfo["expiration_date"]
+    info['contracts'] = parseInt(document.getElementById("details_contracts").value)
+    info['quote_symbol'] = jsonOptionTableInfo['quote_symbol']
+    info['target_price'] = jsonOptionTableInfo['target_price']
+    info['close_price'] = -1
+    info['stock_price'] = -1
+    info['assigned'] = false
+    info['strike_price'] = parseFloat( jsonOptionTableInfo['strike_price'] )
+    info['commisions'] = parseFloat( document.getElementById("details_commision_price").value )
+
+    info['close_date'] = ''
+    info['close_dte'] = -1
+    info['close_pol'] = -1
+    info['sold_price'] = -1
+    info['profit'] = 0
+    info['bs_premium'] = 0
+
+    payload = {}
+    payload['id'] = uuidv4();
+    payload['info'] = info
+    payload['opened'] = true
+    
+    console.log(payload)
+
+    let aws_url = 'https://efd6n53bol.execute-api.us-west-1.amazonaws.com/positions'
+    await putPosition(aws_url,payload)
+
+    document.getElementById("open_button").style.backgroundColor = "#888";
+    document.getElementById("open_button").disabled = true;
+    document.getElementById("open_button").innerHTML = "Opened";
+}
+
+async function putPosition(url,data) {
+    // Awaiting fetch which contains method,
+    // headers and content-type and body
+    console.log(url)
+    console.log(data)
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+           
+    // Awaiting response.json()
+    const resData = await response.json();
+    return resData;
 }
